59bd6e0902f73c8eda5510e4c981f8bb545cca0e
shujiro <shujiro914@outlook.jp>
2022-07-02 12:27:53 +0900

    first commit

---
+++ test.js
@@ -0,0 +1,27 @@
+//1
+var X = 5;
+var Y = 3;
+if (X > Y) {
+  console.log("XはYより大きい");
+}
+
+//2
+var X = 3;
+var Y = 5;
+if (X < Y) {
+  console.log("XはYより小さい");
+}
+
+//3
+var X = 5;
+var Y = 3;
+if (X >= Y) {
+  console.log("XはYより大きいまたは等しい");
+}
+
+4//
+var X = 3;
+var Y = 5;
+if (X <= Y) {
+  console.log("XはYより小さいまたは等しい")
+}
